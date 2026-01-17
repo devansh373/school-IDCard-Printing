03e605cdd120c6adf12d7645e6c2fc84fa735967
Devansh Raghav <Devanshraghavsn@gmail.com>
2026-01-17 16:03:53 +0530

    added logo and template in school response

--- src/controllers/school.controllers.ts
+++ src/controllers/school.controllers.ts
@@ -1,37 +1,13 @@
 import type { Request, Response } from "express";
 import { prisma } from "../db.js";
+import type { AuthRequest } from "../middlewares/authenticate.middleware.js";
+import bcrypt from "bcrypt";
 
-/**
- * Create a school
- */
-// export const createSchool = async (req: Request, res: Response) => {
-//   const { name, code } = req.body;
-
-//   if (!name || !code) {
-//     return res.status(400).json({
-//       message: "School name and code are required",
-//     });
-//   }
-
-//   try {
-//     const school = await prisma.school.create({
-//       data: { name, code },
-//     });
-
-//     return res.status(201).json(school);
-//   } catch (error: any) {
-//     if (error.code === "P2002") {
-//       return res.status(409).json({
-//         message: "School code already exists",
-//       });
-//     }
-
-//     return res.status(500).json({
-//       message: "Failed to create school"+error.message,
-//     });
-//   }
-// };
-
+import { generateTempPassword } from "../utils/password.js";
+import { sendSchoolAdminCredentials } from "../utils/mailer.js";
+import type { Prisma, School } from "../generated/prisma/client.js";
+import { imagekit } from "../config/imagekit.js";
+import ImageKit from "imagekit";
 /**
  * Get all schools with optional filters
  */
@@ -75,7 +51,10 @@ export const getSchools = async (req: Request, res: Response) => {
           registrationDetails: true,
           authoritySignatureUrl: true,
           principalSignatureUrl: true,
+          logoUrl: true,
+          templateUrl: true,
           imagekitPublicKey: true,
+          imagekitPrivateKey: true,
           imagekitUrlEndpoint: true,
           imagekitFolder: true,
           createdAt: true,
@@ -138,6 +117,8 @@ export const getSchoolById = async (req: AuthRequest, res: Response) => {
         registrationDetails: true,
         authoritySignatureUrl: true,
         principalSignatureUrl: true,
+        logoUrl: true,
+        templateUrl: true,
         imagekitPublicKey: true,
         imagekitPrivateKey: true,
         imagekitUrlEndpoint: true,
@@ -172,14 +153,7 @@ export const getSchoolById = async (req: AuthRequest, res: Response) => {
 
 // register school
 
-import type { AuthRequest } from "../middlewares/authenticate.middleware.js";
-import bcrypt from "bcrypt";
 
-import { generateTempPassword } from "../utils/password.js";
-import { sendSchoolAdminCredentials } from "../utils/mailer.js";
-import type { Prisma, School } from "../generated/prisma/client.js";
-import { imagekit } from "../config/imagekit.js";
-import ImageKit from "imagekit";
 
 export const registerSchoolWithAdmin = async (
   req: AuthRequest,
