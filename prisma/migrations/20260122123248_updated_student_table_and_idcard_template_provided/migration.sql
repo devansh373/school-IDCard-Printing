4e91351d62cfcc8ee09752e99d8bd96e56c62633
Devansh Raghav <Devanshraghavsn@gmail.com>
2026-01-23 13:59:50 +0530

    fix aparId column not null

--- prisma/migrations/20260122123248_updated_student_table_and_idcard_template_provided/migration.sql
+++ prisma/migrations/20260122123248_updated_student_table_and_idcard_template_provided/migration.sql
@@ -1,51 +1,41 @@
-/*
-  Warnings:
+-- AlterTable: Step 1 (Add as Nullable)
+ALTER TABLE "Student" ADD COLUMN "aparIdOrPan" TEXT;
+ALTER TABLE "Student" ADD COLUMN "guardianMobileNo" TEXT;
+ALTER TABLE "Student" ADD COLUMN "name" TEXT;
 
-  - You are about to drop the column `aadhar` on the `Student` table. All the data in the column will be lost.
-  - You are about to drop the column `admissionNo` on the `Student` table. All the data in the column will be lost.
-  - You are about to drop the column `aparId` on the `Student` table. All the data in the column will be lost.
-  - You are about to drop the column `email` on the `Student` table. All the data in the column will be lost.
-  - You are about to drop the column `enrollmentNumber` on the `Student` table. All the data in the column will be lost.
-  - You are about to drop the column `fatherName` on the `Student` table. All the data in the column will be lost.
-  - You are about to drop the column `firstName` on the `Student` table. All the data in the column will be lost.
-  - You are about to drop the column `houseName` on the `Student` table. All the data in the column will be lost.
-  - You are about to drop the column `lastName` on the `Student` table. All the data in the column will be lost.
-  - You are about to drop the column `middleName` on the `Student` table. All the data in the column will be lost.
-  - You are about to drop the column `mobileNo` on the `Student` table. All the data in the column will be lost.
-  - You are about to drop the column `motherName` on the `Student` table. All the data in the column will be lost.
-  - You are about to drop the column `pan` on the `Student` table. All the data in the column will be lost.
-  - You are about to drop the column `remarks` on the `Student` table. All the data in the column will be lost.
-  - You are about to drop the column `uniqueId` on the `Student` table. All the data in the column will be lost.
-  - A unique constraint covering the columns `[aparIdOrPan]` on the table `Student` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `aparIdOrPan` to the `Student` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `name` to the `Student` table without a default value. This is not possible if the table is not empty.
+-- Data Migration: Step 2 (Copy values from old columns)
+UPDATE "Student" SET 
+  "name" = TRIM(COALESCE("firstName", '') || ' ' || COALESCE("lastName", '')),
+  "aparIdOrPan" = COALESCE("enrollmentNumber", "aparId", "admissionNo", 'TEMP_' || id::text),
+  "guardianMobileNo" = "mobileNo";
 
-*/
+-- Handle any remaining nulls if columns were completely empty
+UPDATE "Student" SET "name" = 'Unknown Student' WHERE "name" IS NULL OR "name" = '';
+UPDATE "Student" SET "aparIdOrPan" = 'AUTO_' || id::text WHERE "aparIdOrPan" IS NULL;
+
+-- AlterTable: Step 3 (Set Not Null and Drop Columns)
+ALTER TABLE "Student" 
+  ALTER COLUMN "aparIdOrPan" SET NOT NULL,
+  ALTER COLUMN "name" SET NOT NULL,
+  DROP COLUMN "aadhar",
+  DROP COLUMN "admissionNo",
+  DROP COLUMN "aparId",
+  DROP COLUMN "email",
+  DROP COLUMN "enrollmentNumber",
+  DROP COLUMN "fatherName",
+  DROP COLUMN "firstName",
+  DROP COLUMN "houseName",
+  DROP COLUMN "lastName",
+  DROP COLUMN "middleName",
+  DROP COLUMN "mobileNo",
+  DROP COLUMN "motherName",
+  DROP COLUMN "pan",
+  DROP COLUMN "remarks",
+  DROP COLUMN "uniqueId";
+
 -- DropIndex
-DROP INDEX "Student_admissionNo_key";
-
--- DropIndex
-DROP INDEX "Student_enrollmentNumber_key";
-
--- AlterTable
-ALTER TABLE "Student" DROP COLUMN "aadhar",
-DROP COLUMN "admissionNo",
-DROP COLUMN "aparId",
-DROP COLUMN "email",
-DROP COLUMN "enrollmentNumber",
-DROP COLUMN "fatherName",
-DROP COLUMN "firstName",
-DROP COLUMN "houseName",
-DROP COLUMN "lastName",
-DROP COLUMN "middleName",
-DROP COLUMN "mobileNo",
-DROP COLUMN "motherName",
-DROP COLUMN "pan",
-DROP COLUMN "remarks",
-DROP COLUMN "uniqueId",
-ADD COLUMN     "aparIdOrPan" TEXT NOT NULL,
-ADD COLUMN     "guardianMobileNo" TEXT,
-ADD COLUMN     "name" TEXT NOT NULL;
+DROP INDEX IF EXISTS "Student_admissionNo_key";
+DROP INDEX IF EXISTS "Student_enrollmentNumber_key";
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Student_aparIdOrPan_key" ON "Student"("aparIdOrPan");
